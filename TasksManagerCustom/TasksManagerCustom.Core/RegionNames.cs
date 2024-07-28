cb2fc3bd4dd992742b3b2b58d3fc925bcf284b90
Nick Lychagin <flogger19@rambler.ru>
2024-07-28 22:33:05 +0300

    Create regions

--- TasksManagerCustom/TasksManagerCustom.Core/RegionNames.cs
+++ TasksManagerCustom/TasksManagerCustom.Core/RegionNames.cs
@@ -3,5 +3,7 @@
     public static class RegionNames
     {
         public const string ContentRegion = "ContentRegion";
+        public const string TopMenuBar = "TopMenuBar";
+        public const string LeftMenuSpace = "LeftMenuSpace";
     }
 }
