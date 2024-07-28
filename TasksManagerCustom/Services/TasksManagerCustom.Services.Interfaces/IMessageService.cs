f6dac736b50a4f3617b138db6f0456daf7d6aff3
Nick Lychagin <flogger19@rambler.ru>
2024-07-28 12:36:55 +0300

    Add project files.

---
+++ TasksManagerCustom/Services/TasksManagerCustom.Services.Interfaces/IMessageService.cs
@@ -0,0 +1,7 @@
+﻿namespace TasksManagerCustom.Services.Interfaces
+{
+    public interface IMessageService
+    {
+        string GetMessage();
+    }
+}
---
+++ TasksManagerCustom/Services/TasksManagerCustom.Services/MessageService.cs
@@ -0,0 +1,12 @@
+﻿using TasksManagerCustom.Services.Interfaces;
+
+namespace TasksManagerCustom.Services
+{
+    public class MessageService : IMessageService
+    {
+        public string GetMessage()
+        {
+            return "Hello from the Message Service";
+        }
+    }
+}
